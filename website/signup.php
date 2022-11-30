431be777b689094b471f09b6e7ec34abdadec6a4
MutaoYin <72797020+Yin-Marina@users.noreply.github.com>
2022-11-30 16:01:49 -0500

    sign up

--- website/signup.php
+++ website/signup.php
@@ -3,89 +3,98 @@
 require_once "./php/connection.php";
  
 // Define variables and initialize with empty values
-$first_name = $last_name = $username = $password = $confirm_password = "";
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$confirm_password = $_GET['confirm_password'];
 $first_name_err = $last_name_err = $username_err = $password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Validate username
-    if(empty(trim($_POST["email"]))){
-        $username_err = "Please enter your email.";
-    } elseif(!preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', trim($_POST["email"]))){
-        $username_err = "Please enter a vaild email.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
+    // if(empty(trim($_POST["email"]))){
+    //     $username_err = "Please enter your email.";
+    // } elseif(!preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/', trim($_POST["email"]))){
+    //     $username_err = "Please enter a vaild email.";
+    // } else{
+    //     // Prepare a select statement
+    //     $sql = "SELECT email FROM users WHERE email = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+    //     if($stmt = mysqli_prepare($con, $sql)){
+    //         // Bind variables to the prepared statement as parameters
+    //         mysqli_stmt_bind_param($stmt, "s", $param_email);
             
-            // Set parameters
-            $param_username = trim($_POST["username"]);
+    //         // Set parameters
+    //         $param_email = trim($_POST["email"]);
             
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
+    //         // Attempt to execute the prepared statement
+    //         if(mysqli_stmt_execute($stmt)){
+    //             /* store result */
+    //             mysqli_stmt_store_result($stmt);
                 
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This email is already taken.";
-                } else{
-                    $username = trim($_POST["email"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+    //             if(mysqli_stmt_num_rows($stmt) == 1){
+    //                 $email_err = "This email is already taken.";
+    //             } else{
+    //                 $email = trim($_POST["email"]);
+    //             }
+    //         } else{
+    //             echo "Oops! Something went wrong. Please try again later.";
+    //         }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
+    //         // Close statement
+    //         //mysqli_stmt_close($stmt);
+    //     }
+    // }
     // Validate first name
-    if(empty(trim($_POST["firstName"]))){
-        $first_name_err = "Please enter your first name.";
-    }
+    // if(empty(trim($_POST["first_name"]))){
+    //   $first_name_err = "Please enter your first name.";
+    // } else {
+    //   $param_first_name = trim($_POST["first_name"]);
+    // }
 
 
-    // Validate last name
-    if(empty(trim($_POST["lastName"]))){
-        $last_name_err = "Please enter your last name.";
-    }
+    // // Validate last name
+    // if(empty(trim($_POST["last_name"]))){
+    //   $last_name_err = "Please enter your last name.";
+    // } else {
+    //   $param_last_name = trim($_POST["last_name"]);
+    // }
 
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have at least 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
+    // // Validate password
+    // if(empty(trim($_POST["password"]))){
+    //     $password_err = "Please enter a password.";     
+    // } elseif(strlen(trim($_POST["password"])) < 6){
+    //     $password_err = "Password must have at least 6 characters.";
+    // } else{
+    //     $password = trim($_POST["password"]);
+    // }
     
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
+    // // Validate confirm password
+    // if(empty(trim($_POST["confirm_password"]))){
+    //     $confirm_password_err = "Please confirm password.";     
+    // } elseif (empty($password_err) && ($password != $confirm_password)){
+    //     $confirm_password_err = "Password did not match.";
+    // } else {
+    //     $confirm_password = trim($_POST["confirm_password"]);
+    // }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($first_name_err) && empty($last_name_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (firstName, lastname, email, password) VALUES (?, ?, ?, ?)";
          
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = mysqli_prepare($con, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "ssss", $first_name, $last_name, $email, $password);
             
             // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_email = $email;
+
+            $param_password = $password; // pass in the password without encryption
+            // $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -96,12 +105,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
 
             // Close statement
-            mysqli_stmt_close($stmt);
+            //mysqli_stmt_close($stmt);
         }
     }
     
     // Close connection
-    mysqli_close($link);
+    mysqli_close($con);
 }
 ?>
 
@@ -154,38 +163,68 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <div class="container">
     <div class="wrap">
       <h1>Sign Up</h1>
-      <form onsubmit="return validateForm();" action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+      <form action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <div class="field">
-          <input placeholder="First Name" type="text" id="firstName">
-          <p id="firstNameError"></p>
+          <input placeholder="First Name" type="text" id="first_name"
+          class="form-control"
+          name="first_name">
+          <p id="firstNameError">
+          <?php echo $first_name_err; ?>
+          </p>
         </div>
         <div class="field">
           <input placeholder="Last Name" type="text" id="last_name" 
-          class="form-control <?php echo (!empty($last_name_err)) ? 'is-invalid' : ''; ?>" 
+          class="form-control" 
           value="<?php echo $last_name; ?>"
           name="last_name">
-          <p id="lastNameError" class="invalid-feedback"><?php echo $last_name_err; ?></p>
+          <p id="lastNameError" class="invalid-feedback">
+            <?php echo $last_name_err; ?>
+          </p>
         </div>
         <div class="field">
-          <input placeholder="Email" type="text" id="email">
-          <p id="emailError"></p>
+          <input placeholder="Email" type="text" id="email"
+          class="form-control"
+          name="email">
+          <p id="emailError">
+          <?php echo $email_err; ?>
+          </p>
         </div>
         <div class="field">
-          <input placeholder="Password" type="password" id="password">
-          <p id="passwordError"></p>
+          <input placeholder="Password" type="password" id="password"
+          class="form-control"
+          name="password">
+          <p id="passwordError">
+          <?php echo $password_err; ?>
+          </p>
         </div>
         <div class="field">
-          <input placeholder="Confirm Password" type="Password" id="confirm_password">
-          <p id="confirmPasswordError"></p>
+          <input placeholder="Confirm Password" type="Password" id="confirm_password"
+          class="form-control"
+          name="confirm_password">
+          <p id="confirmPasswordError">
+          <?php echo $confirm_password_err; ?>
+          </p>
         </div>
-        <button id="submit">Sign up</button>
+
+        // debugging div
+        <div>
+        <?php
+        
+        
+
+        ?>
+        </div>
+
+
+
+        <button id="submit" type="submit">Sign up</button>
       </form>
-      <p class="footer">Already have an account?
+      <p class="">Already have an account?
         <a class="link" href="login.html">Login</a>
       </p>
     </div>
   </div>
-  <script type="text/javascript" src="js/auth.js"></script>
+  <script type="text/javascript" src=""></script>
   <!-- Bootstrap Latest compiled and minified JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
     integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
