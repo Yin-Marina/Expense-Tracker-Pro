fa2f6f8c35c16cb84efac3039d574fee24a2a4c3
MutaoYin <72797020+Yin-Marina@users.noreply.github.com>
2022-12-02 13:15:22 -0500

    header for user-system web pages
    Modified header.php, relocate it to ./php/ apply it to all user-system webpages: dashboard, profile, insert and report

--- website/report.php
+++ website/report.php
@@ -1,117 +1,159 @@
 <?php
-  include "header.php" ;
+// start the session
+session_start();
+// If the user is not logged in redirect to the login page.
+if (!isset($_SESSION['loggedin'])) {
+  header('Location: index.html');
+  exit;
+}
+
+require "./php/connection.php";
+
+
+
+mysqli_close($con);
 ?>
-    <title>Report</title>
-    <link rel="stylesheet" type="text/css" href="css/report.css">
-    <script type="text/javascript" src="js/report.js" defer></script>
-  </head>
-  <body>
-    <?php
-      include("nav.php");
-      require_once('php/database.php');
+<!DOCTYPE html>
+<html lang="en">
 
-      $query = '';
-      $from = '';
-      $to = '';
-      $sortType = '';
-      $sortAmount = '';
+<head>
+  <!-- Apply website universal header -->
+  <?php
+  require_once "./php/header.php";
+  ?>
 
-      $db = db_connect();
-      $sql = "SELECT transactions.id, date, amount, notes, name FROM transactions"
-          . " join types on transactions.typeId = types.id and transactions.userId = " . $_SESSION['id'];
-      if (isset($_GET['submit'])) {
-        $query = $_GET['query'];
-        $from = $_GET['from'];
-        $to = $_GET['to'];
-        $sortType = $_GET['sortType'];
-        $sortAmount = $_GET['sortAmount'];
+  <title>input</title>
 
-        if (!empty($query)) {
-          $sql .= " and transactions.notes like '%$query%'"
-              . " and types.name like '%$query%'";
-        }
-        if (!empty($from)) {
-          $sql .= " and transactions.date >= '$from'";
-        }
-        if (!empty($to)) {
-          $sql .= " and transactions.date <= '$to'";
-        }
-        $sorts = [];
-        if (!empty($sortType)) {
-          array_push($sorts, "types.name $sortType");
-        }
-        if (!empty($sortAmount)) {
-          array_push($sorts, "transactions.amount $sortAmount");
-        }
-        if (count($sorts) > 0) {
-          $sql .= " order by " . implode(', ', $sorts);
-        }
-      }
-      $results = mysqli_query($db, $sql);
-    ?>
+  <!-- Page specific stylesheet -->
+  <link rel="stylesheet" type="text/css" href="./css/report.css" />
+
+  <!-- Page specific javascript for validate -->
+  <script type="text/javascript" src="js/report.js" defer></script>
+</head>
+
+<body>
+  <?php
+  include("./php/nav_inner.php");
+  require_once('php/database.php');
+
+  $query = '';
+  $from = '';
+  $to = '';
+  $sortType = '';
+  $sortAmount = '';
+
+  $db = db_connect();
+  $sql = "SELECT transactions.id, date, amount, notes, name FROM transactions"
+    . " join types on transactions.typeId = types.id and transactions.userId = " . $_SESSION['id'];
+  if (isset($_GET['submit'])) {
+    $query = $_GET['query'];
+    $from = $_GET['from'];
+    $to = $_GET['to'];
+    $sortType = $_GET['sortType'];
+    $sortAmount = $_GET['sortAmount'];
+
+    if (!empty($query)) {
+      $sql .= " and transactions.notes like '%$query%'"
+        . " and types.name like '%$query%'";
+    }
+    if (!empty($from)) {
+      $sql .= " and transactions.date >= '$from'";
+    }
+    if (!empty($to)) {
+      $sql .= " and transactions.date <= '$to'";
+    }
+    $sorts = [];
+    if (!empty($sortType)) {
+      array_push($sorts, "types.name $sortType");
+    }
+    if (!empty($sortAmount)) {
+      array_push($sorts, "transactions.amount $sortAmount");
+    }
+    if (count($sorts) > 0) {
+      $sql .= " order by " . implode(', ', $sorts);
+    }
+  }
+  $results = mysqli_query($db, $sql);
+  ?>
 
   <div class="container">
     <form method="get" action="report.php">
-        <div class="searchWrap">
-          <div class="search">
-            <input type="text" class="searchInput" placeholder="Search for expenses" name="query" value="<?php echo $query; ?>">
-            <button class="searchButton" type="submit" name="submit">Search</button>
+      <div class="searchWrap">
+        <div class="search">
+          <input type="text" class="searchInput" placeholder="Search for expenses" name="query"
+            value="<?php echo $query; ?>">
+          <button class="searchButton" type="submit" name="submit">Search</button>
+        </div>
+      </div>
+      <div class="filters">
+        <div class="filter">
+          <div class="from">
+            <label for="from">From:</label>
+            <input id="from" type="date" name="from" value="<?php echo $from; ?>"
+              max="<?php echo empty($to) ? '' : $to; ?>">
+          </div>
+          <div class="to">
+            <label for="to">To:</label>
+            <input id="to" type="date" name="to" value="<?php echo $to; ?>"
+              min="<?php echo empty($from) ? '' : $from; ?>">
           </div>
         </div>
-        <div class="filters">
-          <div class="filter">
-            <div class="from">
-              <label for="from">From:</label>
-              <input id="from" type="date" name="from" value="<?php echo $from; ?>" max="<?php echo empty($to) ? '' : $to; ?>">
-            </div>
-            <div class="to">
-              <label for="to">To:</label>
-              <input id="to" type="date" name="to" value="<?php echo $to; ?>" min="<?php echo empty($from) ? '' : $from; ?>">
-            </div>
+        <div class="sorts">
+          <div class="sort">
+            <label for="sortType">Sort type by:</label>
+            <select name="sortType" id="sortType">
+              <option value="" <?php echo empty($sortType) ? 'selected' : ''; ?>>---</option>
+              <option value="asc" <?php echo $sortType === 'asc' ? 'selected' : ''; ?>>Ascending order</option>
+              <option value="desc" <?php echo $sortType === 'desc' ? 'selected' : ''; ?>>Descending order</option>
+            </select>
           </div>
-          <div class="sorts">
-            <div class="sort">
-              <label for="sortType">Sort type by:</label>
-              <select name="sortType" id="sortType">
-                <option value="" <?php echo empty($sortType) ? 'selected' : ''; ?>>---</option>
-                <option value="asc" <?php echo $sortType === 'asc' ? 'selected' : ''; ?>>Ascending order</option>
-                <option value="desc" <?php echo $sortType === 'desc' ? 'selected' : ''; ?>>Descending order</option>
-              </select>
-            </div>
-            <div class="sort">
-              <label for="sortAmount">Sort amount by:</label>
-              <select name="sortAmount" id="sortAmount">
-                <option value="" <?php echo empty($sortAmount) ? 'selected' : ''; ?>>---</option>
-                <option value="asc" <?php echo $sortAmount === 'asc' ? 'selected' : ''; ?>>Lowest to highest</option>
-                <option value="desc" <?php echo $sortAmount === 'desc' ? 'selected' : ''; ?>>Highest to lowest</option>
-              </select>
-            </div>
+          <div class="sort">
+            <label for="sortAmount">Sort amount by:</label>
+            <select name="sortAmount" id="sortAmount">
+              <option value="" <?php echo empty($sortAmount) ? 'selected' : ''; ?>>---</option>
+              <option value="asc" <?php echo $sortAmount === 'asc' ? 'selected' : ''; ?>>Lowest to highest</option>
+              <option value="desc" <?php echo $sortAmount === 'desc' ? 'selected' : ''; ?>>Highest to lowest</option>
+            </select>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
+  </div>
 
-    <div class="container">
-      <table>
-        <tr>
-          <th>Date</th>
-          <th>Amount</th>
-          <th>Type</th>
-          <th>Memo</th>
-          <th>&nbsp</th>
-          <th>&nbsp</th>
-        </tr>
-        <?php while ($row = mysqli_fetch_assoc($results)) { ?>
-          <tr>
-            <td><?php echo $row['date'] ?></td>
-            <td>$<?php echo $row['amount'] ?></td>
-            <td><?php echo $row['name'] ?></td>
-            <td><?php echo $row['notes'] ?></td>
-            <td><a href="<?php echo "report_edit.php?id=" . $row['id']; ?>">Edit</a></td>
-            <td><a href="<?php echo "report_delete.php?id=" . $row['id']; ?>">delete</a></td>
-          </tr>
-        <?php } ?>
-      </table>
-    </div>
-  </body>
+  <div class="container">
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Amount</th>
+        <th>Type</th>
+        <th>Memo</th>
+        <th>&nbsp</th>
+        <th>&nbsp</th>
+      </tr>
+      <?php while ($row = mysqli_fetch_assoc($results)) { ?>
+      <tr>
+        <td>
+          <?php echo $row['date'] ?>
+        </td>
+        <td>$
+          <?php echo $row['amount'] ?>
+        </td>
+        <td>
+          <?php echo $row['name'] ?>
+        </td>
+        <td>
+          <?php echo $row['notes'] ?>
+        </td>
+        <td><a href="<?php echo "report_edit.php?id=" . $row['id']; ?>">Edit</a></td>
+        <td><a href="<?php echo "report_delete.php?id=" . $row['id']; ?>">delete</a></td>
+      </tr>
+      <?php } ?>
+    </table>
+  </div>
+  <!-- footer -->
+  <?php
+  require "./php/footer_outer.php"
+    ?>
+</body>
+
 </html>
--- website/welcome.php
+++ website/welcome.php
@@ -30,33 +30,15 @@ $stmt2->close();
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<title>Welcome</title>
+	<!-- Apply website universal header -->
+	<?php
+    require_once "./php/header.php";
+    ?>
 
-	<!-- Bootstrap Latest compiled and minified CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
-
-	<!--  Bootstrap Optional theme -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css"
-		integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Header/footer Stylesheet -->
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>input</title>
 
 	<!-- Page specific stylesheet -->
-	<link rel="stylesheet" type="text/css" href="css/index.css" />
-
-	<link rel="icon" href="favicon.ico" />
-
-	<!-- Bootstrap Latest JavaScript -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-		integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-		crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-		integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-		crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="" />
 
 </head>
 
