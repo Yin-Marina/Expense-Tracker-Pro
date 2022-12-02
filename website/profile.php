90b0fe5df78969c7108823f941e1af7452e31a25
MutaoYin <72797020+Yin-Marina@users.noreply.github.com>
2022-12-02 12:40:49 -0500

    Update profile.php
    for unified nav/footer

--- website/profile.php
+++ website/profile.php
@@ -1,34 +1,29 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
+// start the session
 session_start();
-// If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page.
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
 }
+
 require "./php/connection.php";
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
+
+// Query the Database for user information that need to be displayed in the profile page.
 $stmt = $con->prepare('SELECT firstName, lastName, password, email FROM users WHERE id = ?');
-// In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
 $stmt->bind_result($firstName, $lastName, $password, $email);
 $stmt->fetch();
 $stmt->close();
 ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
 	<meta charset="utf-8">
 	<title>Profile Page</title>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
-		rel="stylesheet" />
-
-
 	<!-- Bootstrap Latest compiled and minified CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
@@ -39,39 +34,27 @@ $stmt->close();
 
 	<!-- Header/footer Stylesheet -->
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
-	<link rel="stylesheet" type="text/css" href="css/font-style.css" />
+
+	<!-- Page specific stylesheet -->
 	<link rel="stylesheet" type="text/css" href="css/index.css" />
+
+	<link rel="icon" href="favicon.ico" />
+
+	<!-- Bootstrap Latest JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+		integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+		integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+		crossorigin="anonymous"></script>
 </head>
 
 <body class="loggedin">
 	<!--Nav bar-->
-	<nav class="outer-interface navbar navbar-expand-xl bg-info Sticky-top px-5 py-5">
-		<a class="navbar-brand" href="#">
-			<span class="h2 text-light">
-				<p>Welcome back, <?= $firstName, " ", $lastName ?>!</p>
-			</span>
-		</a>
-		<div class="collapse navbar-collapse d-flex justify-content-end ">
-			<ul class="navbar-nav mr-auto d-flex justify-content-end pull-right h4">
-				<li class="nav-item active">
-					<a class="nav-link text-light h3" href="./welcome.php">User Home</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link text-light h3" href="profile.php">Profile</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link text-light h3" href="insert.html">New Spending</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link text-light h3" href="report.php">View Report</a>
-				</li>
-				<li class="nav-item active">
-					<a class="nav-link text-light h3" href="logout.php">Logout</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
-
+	<?php
+    require "./php/nav_inner.php"
+    	?>
 	
 	<div class="card">
 		<h2 class="card-header">Account Detail:</h2>
@@ -103,14 +86,11 @@ $stmt->close();
 		</div>
 	</div>
 
-	<!--Footer-->
-	<footer class="footer fixed-bottom d-flex flex-row-reverse bg-info ">
-		<p class="p-2 bd-highlight flex-end text-light">2022 Expense Tracker Pro</p>
-	</footer>
-	<!-- Bootstrap Latest compiled and minified JavaScript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
-		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-		crossorigin="anonymous"></script>
+	<!-- footer -->
+	<?php
+    require "./php/footer_outer.php"
+    	?>
+
 </body>
 
 </html>
